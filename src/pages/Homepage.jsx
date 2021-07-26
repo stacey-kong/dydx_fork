8c8e1e383d55daf882b9982171dd76cd4272c5b5
stacey <727@stlnd.net>
2021-07-26 17:26:56 +0800

    add device detect package to load video backgroungd only on desktop device

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,11 @@
+import { isMobile } from "react-device-detect";
 import Video from "../component/VideoBackground";
 import Header from "../component/Header/Header";
 
 export default function Homepage() {
   return (
     <div className="homepageWrapper min-h-screen text-white ">
-      <Video />
+      {!isMobile && <Video />}
       <div className="fixedBar sticky bg-white top-0 w-full h-10 z-10"></div>
       <div className="pageContainer w-9/10 m-auto mt-20">
         <Header />

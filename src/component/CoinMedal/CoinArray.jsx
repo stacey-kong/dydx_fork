c7678e122931fc1d2c0e28e061b4d869723641b5
stacey <727@stlnd.net>
2021-07-27 17:38:29 +0800

    add coin array with icon and fake market data

---
+++ src/component/CoinMedal/CoinArray.jsx
@@ -0,0 +1,183 @@
+import eth from "../../images/Coin/eth.svg";
+import btc from "../../images/Coin/btc.svg";
+import ada from "../../images/Coin/ada.svg";
+import doge from "../../images/Coin/doge.svg";
+import dot from "../../images/Coin/dot.svg";
+import link from "../../images/Coin/link.svg";
+import uni from "../../images/Coin/uni.svg";
+import sol from "../../images/Coin/sol.svg";
+import matic from "../../images/Coin/matic.svg";
+import aave from "../../images/Coin/aave.svg";
+import atom from "../../images/Coin/atom.svg";
+import mkr from "../../images/Coin/mkr.svg";
+import comp from "../../images/Coin/comp.svg";
+import avax from "../../images/Coin/avax.svg";
+import snx from "../../images/Coin/snx.svg";
+import sushi from "../../images/Coin/sushi.svg";
+import yfi from "../../images/Coin/yfi.svg";
+import uma from "../../images/Coin/uma.svg";
+import crv from "../../images/Coin/crv.svg";
+import inch from "../../images/Coin/1inch.svg";
+
+export const coinArray = [
+  {
+    name: "Ethereum",
+    symbol: "eth",
+    price: "2,181.15",
+    change: "-5.47",
+    volumn: "33.5",
+    iconSrc: eth,
+  },
+  {
+    name: "Bitcoin",
+    symbol: "btc",
+    price: "36,853.05",
+    change: "-3.46",
+    volumn: "12.8",
+    iconSrc: btc,
+  },
+  {
+    name: "Cardano",
+    symbol: "ada",
+    price: "1.23",
+    change: "-7.1",
+    volumn: "0.0",
+    iconSrc: ada,
+  },
+  {
+    name: "Dogecoin",
+    symbol: "doge",
+    price: "0.20",
+    change: "-11.08",
+    volumn: "0.6",
+    iconSrc: doge,
+  },
+  {
+    name: "Polkadot",
+    symbol: "dot",
+    price: "13.67",
+    change: "-7.93",
+    volumn: "0.7",
+    iconSrc: dot,
+  },
+  {
+    name: "Chainlink",
+    symbol: "link",
+    price: "18.05",
+    change: "-2.68",
+    volumn: "0.2",
+    iconSrc: link,
+  },
+  {
+    name: "Uniswap",
+    symbol: "uni",
+    price: "18.04",
+    change: "-7.78",
+    volumn: "0.2",
+    iconSrc: uni,
+  },
+  {
+    name: "Solana",
+    symbol: "sol",
+    price: "27.50",
+    change: "-8.47",
+    volumn: "0.3",
+    iconSrc: sol,
+  },
+  {
+    name: "Polygon",
+    symbol: "matic",
+    price: "0.96",
+    change: "-6.84",
+    volumn: "1.1",
+    iconSrc: matic,
+  },
+  {
+    name: "Aave",
+    symbol: "aave",
+    price: "293.91",
+    change: "-6.79",
+    volumn: "0.6",
+    iconSrc: aave,
+  },
+  {
+    name: "Cosmos",
+    symbol: "atom",
+    price: "11.14",
+    change: "-8.50",
+    volumn: "0.1",
+    iconSrc: atom,
+  },
+  {
+    name: "Maker",
+    symbol: "mkr",
+    price: "2,534.50",
+    change: "-7.42",
+    volumn: "0.1",
+    iconSrc: mkr,
+  },
+  {
+    name: "Compound",
+    symbol: "comp",
+    price: "382.26",
+    change: "-8.18",
+    volumn: "0.1",
+    iconSrc: comp,
+  },
+  {
+    name: "Avalanche",
+    symbol: "avax",
+    price: "11.30",
+    change: "-6.16",
+    volumn: "0.2",
+    iconSrc: avax,
+  },
+  {
+    name: "Synthesix",
+    symbol: "snx",
+    price: "8.95",
+    change: "-1.98",
+    volumn: "0.2",
+    iconSrc: snx,
+  },
+  {
+    name: "SushiSwap",
+    symbol: "sushi",
+    price: "8.22",
+    change: "-14.46",
+    volumn: "0.1",
+    iconSrc: sushi,
+  },
+  {
+    name: "Yearn",
+    symbol: "yfi",
+    price: "29,726.97",
+    change: "-8.41",
+    volumn: "0.1",
+    iconSrc: yfi,
+  },
+  {
+    name: "UMA",
+    symbol: "uma",
+    price: "8.47",
+    change: "-7.91",
+    volumn: "0.1",
+    iconSrc: uma,
+  },
+  {
+    name: "Curve",
+    symbol: "crv",
+    price: "1.53",
+    change: "-10.03",
+    volumn: "0.3",
+    iconSrc: crv,
+  },
+  {
+    name: "1inch",
+    symbol: "1inch",
+    price: "2.16",
+    change: "-5.38",
+    volumn: "0.5",
+    iconSrc: inch,
+  },
+];
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,14 +1,16 @@
 import { isMobile } from "react-device-detect";
 import Video from "../component/VideoBackground";
 import Header from "../component/Header/Header";
+import Market from "../component/Market/Market";
 
 export default function Homepage() {
   return (
     <div className="homepageWrapper min-h-screen text-white ">
       {!isMobile && <Video />}
       <div className="fixedBar sticky bg-white top-0 w-full h-10 z-10"></div>
-      <div className="pageContainer w-9/10 m-auto mt-20">
+      <div className="pageContainer w-9/10 m-auto">
         <Header />
+        <Market />
       </div>
     </div>
   );
